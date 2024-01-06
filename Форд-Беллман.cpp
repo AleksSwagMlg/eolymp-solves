90241551c1cf6a6ea45dbc9d8d58a1c487972d55
Oleksii <122645716+AleksSwagMlg@users.noreply.github.com>
2024-01-06 18:10:50 +0200

    Add cpp solve
    Only cpp solution works on the site

---
+++ Форд-Беллман.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+#define MAX 10002
+#define INF 0x3F3F3F3F
+
+int k, i, n, m, x[MAX], y[MAX], w[MAX], d[MAX];
+
+void Bellman_Ford()
+{
+	int i, j;
+	memset(d, 0x3F, sizeof d);
+	d[1] = 0;
+	for (i = 1; i <= n; i++)
+		for (j = 1; j <= m; j++)
+			if (d[x[j]] < INF && d[y[j]] > d[x[j]] + w[j]) d[y[j]] = d[x[j]] + w[j];
+}
+
+int main()
+{
+	scanf_s("%d %d", &n, &m);
+	for (i = 1; i <= m; i++) scanf_s("%d %d %d", &x[i], &y[i], &w[i]);
+	Bellman_Ford();
+	for (i = 1; i <= n; i++)
+		if (d[i] == INF) d[i] = 30000;
+	for (i = 1; i < n; i++) printf("%d ", d[i]);
+	printf("%d\n", d[n]);
+	return 0;
+}
